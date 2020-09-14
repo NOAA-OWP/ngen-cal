af2733c0a3d4723c42bd646660d80e31ec19c651
Nels Frazier <nels.frazier@noaa.gov>
2020-09-14 16:27:00 -0500

    initial test and test data

---
+++ python/ngen_cal/test/test_configuration.py
@@ -0,0 +1,26 @@
+import pytest
+from typing import Generator
+from pathlib import Path
+
+from ngen_cal import Configuration
+
+"""
+    Test suite for reading and manipulating ngen configration files
+"""
+_current_dir = Path(__file__).resolve().parent
+
+@pytest.fixture
+def conf() -> Generator[Configuration, None, None]:
+    """
+        Staging of a generator to test
+    """
+    test_config_file = _current_dir.joinpath('data/example_realization_config.json')
+
+    yield Configuration(test_config_file)
+
+def test_configuration(conf: Configuration):
+    """
+        Test configuration is properly constructed
+    """
+    assert conf.data != None
+    assert len(conf._catchments) != 0
