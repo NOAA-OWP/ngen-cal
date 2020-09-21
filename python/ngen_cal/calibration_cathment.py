9da54327f766881cae886dc9ab524b22619cc780
Nels Frazier <nels.frazier@noaa.gov>
2020-09-21 09:36:20 -0500

    add output and observed properties to CalibrationCatchment

--- python/ngen_cal/calibration_cathment.py
+++ python/ngen_cal/calibration_cathment.py
@@ -23,6 +23,9 @@ class CalibrationCatchment(Catchment, Calibratable):
         #FIXME paramterize
         self._output_file = 'test_file.out'
 
+        self._observed = DataFrame()
+        self._output = DataFrame()
+
     @property
     def df(self) -> 'DataFrame':
         """
@@ -41,3 +44,39 @@ class CalibrationCatchment(Catchment, Calibratable):
             update configuration based on latest params
         """
         pass
+
+    @property
+    def output(self) -> 'DataFrame':
+        """
+            The model output hydrograph for this catchment
+        """
+        #FIXME read the correct ngen output structure nex-X_output.csv
+        #simulated_hydrograph_file = workdir+"{}_output.csv".format(self.id)
+        #hydrograph = pd.read_csv(simulated_hydrograph_file, header=None, usecols=[0,1], names=['time', 'flow'])
+        #hydrograph['time'] /= 86400 #partial day
+        #hydrograph['time'] += hydrograph_reference_date #Julian date from reference
+        #hydrograph['time'] = pd.to_datetime(simulated_hydrograph['time'], utc=True, unit='D', origin='julian').dt.round('1s')
+        #hydrograph.drop_duplicates('time', keep='last', inplace=True)
+        #hydrograph.set_index('time', inplace=True)
+        hydrograph = self._output
+        return hydrograph
+
+    @property
+    def observed(self) -> 'DataFrame':
+        """
+            The observed hydrograph for this catchment FIXME set up in __init__ move output/observed to calibratable
+        """
+        #FIXME hook to NWIS
+        #observed_file = os.path.join(config.workdir, 'usgs_{}_observed.csv'.format(usgs))
+        #observed_df = pd.read_csv(observed_file, parse_dates=['Date_Time']).set_index('Date_Time')
+        #observed_df = observed_df.tz_localize('UTC')
+        hydrograph = self._observed
+        return hydrograph
+
+    @observed.setter
+    def observed(self, df):
+        self._observed = df
+    @output.setter
+    def output(self, df):
+        self._output = df
+
