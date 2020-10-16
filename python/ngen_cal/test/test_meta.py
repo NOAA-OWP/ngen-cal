22eb5c80e7a1fb95c872825f071563260436abc4
Nels Frazier <nels.frazier@noaa.gov>
2020-10-16 14:01:45 -0500

    tests for meta class

---
+++ python/ngen_cal/test/test_meta.py
@@ -0,0 +1,103 @@
+import pytest
+from typing import Generator
+from pathlib import Path
+import json
+import pandas as pd
+
+from ..configuration import Configuration
+from ..meta import CalibrationMeta
+from .utils import config
+
+"""
+    Test suite for reading and manipulating ngen configration files
+"""
+
+
+@pytest.fixture(scope="session")
+def realization_config(tmpdir_factory):
+    """
+        Fixture to provide a staged testing input files
+    """
+    fn = tmpdir_factory.mktemp("data").join("realization_config.json")
+    with(open(fn, 'w')) as fp:
+        json.dump(config, fp)
+    return fn
+
+@pytest.fixture
+def conf(realization_config) -> Generator[Configuration, None, None]:
+    """
+        Staging of a generator to test
+    """
+    print(type(realization_config))
+    print(realization_config)
+    yield Configuration(realization_config)
+
+@pytest.fixture
+def meta(conf, tmpdir_factory):
+    """
+        build up a meta object to test
+    """
+    bin = "echo ngen"
+    args = "args"
+    workdir = tmpdir_factory.mktemp("workdir")
+    m = CalibrationMeta(conf, workdir, bin, args, "test_0")
+    yield m
+
+def test_update_config(meta, realization_config):
+    """
+        Ensure that update config properly updates and serializes the config
+    """
+    i = 0
+    params = pd.DataFrame({"0":4.2, "param":"some_param"}, index=[0])
+    id = 'test-catchment'
+    meta.update_config(i, params, id)
+    with open(realization_config) as fp:
+        data = json.load(fp)
+    assert data['catchments'][id]['formulations'][0]['params']['some_param'] == 4.2
+
+def test_update(meta):
+    """
+        Test score update function with a worse score
+    """
+    meta._best_score = 0.5
+    i = 1
+    score = 1.0
+    log = False
+    meta.update(i, score, log)
+    assert meta.best_score == 0.5
+    assert meta.best_params == '0'
+
+def test_update_1(meta):
+    """
+        Test score update function with a better score
+    """
+    meta._best_score = 1
+    i = 1
+    score = 0.1
+    log = False
+    meta.update(i, score, log)
+    assert meta.best_score == 0.1
+    assert meta.best_params == '1'
+
+def test_restart(meta):
+    """
+        Test restarting from minimal meta, no logs available
+        should "restart" at iteration 0
+    """
+    iteration = meta.restart()
+    assert iteration == 0
+
+def test_restart(meta):
+    """
+        Test retarting from serialized logs
+    """
+    meta._best_score = 1
+    meta._best_param_iteration = "1"
+    meta.write_param_log_file(2)
+    #make sure the catchment param df is saved before trying to restart
+    meta._config.catchments[0].check_point(meta._workdir)
+
+    iteration = meta.restart()
+    assert iteration == 3
+    assert meta.best_score == 1
+    assert meta.best_params == '1'
