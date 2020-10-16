7787797c059ded4df7cae95bc7ddc6a388b5d638
Nels Frazier <nels.frazier@noaa.gov>
2020-10-16 13:52:59 -0500

    simplified update_config

--- python/ngen_cal/meta.py
+++ python/ngen_cal/meta.py
@@ -31,7 +31,7 @@ class CalibrationMeta:
         self._param_log_file = self._workdir/"{}_best_params.log".format(self._id)
         self._objective_log_file = self._workdir/"{}_objective.log".format(self._id)
 
-    def update_config(self, i: int, params: 'DataFrame', cat_id: str):
+    def update_config(self, i: int, params: 'DataFrame', id: str):
         """
             For a given calibration iteration, i, update the input files/configuration to prepare for that iterations
             calibration run.
@@ -44,75 +44,23 @@ class CalibrationMeta:
                 DataFrame containing the parameter name in `param` and value in `i` columns
         """
 
-        #extract parameters from Pandas DataFrame data to update JSON configuration file 
-        #for next round of iteration run
-        print(params, "\n")
-        """
-        # extracting parameter name and value columns from a dataframe params and convert to dict for easy access
-        # assuming the column order is (param, i)
-        param_dict = dict()
-        for idx, p in params.iterrows():
-            print("idx = {}".format(idx))
-            print("params column name and values = {}".format(p.values))
-            #print("params column name and values = {}".format(p.values[0]))    # use for calibration.csv with no commas
-            #p_list = (p.values[0]).split()    # use for calibration.csv with no commas, this insert a comma between values
-            p_list = p.values
-            print("p_list = {}".format(p_list))
-            dict_tmp = {p_list[0]: float(p_list[-1])}
-            param_dict.update(dict_tmp)
-            print("p_list[2] = {}".format(p_list[2]))
-            print(dict_tmp)
-        """
-
-        # extracting parameter value and name columns from a dataframe params and convert to dict for easy access
-        # assuming the column order is (i, param)
-        param_dict = dict()
-        for idx, p in params.iterrows():
-            dict_tmp = {(p.values[-1]).strip(): float(p.values[0])}
-            print("dict_tmp = {}".format(dict_tmp))
-            param_dict.update(dict_tmp)
-
-        # print out the extracted parameters in dictionary form
-        print("\nparam_dict:", param_dict)
-        print("\n")
-
         # update the config file using the best estimate for parameters in the last calibration step
-        # calibration_config.json is the input file containing the calibration parameters for te catchments
-        #config_file = "calibration_config.json"
-        config_file = Configuration(config_file, '')
-        config_in = config_file
-        #using alternative config_in, check that the output can be read back in
-        #config_file = "config_out1.json"
-
+        #config object makes a backup of the original, so we just overwrite the existing one to prepare for the next step
         # read in the config file from last calibration step
-        #with open(config_file) as fp:
-        with open(config_in) as fp:
+        with open(self._config.config_file, 'r') as fp:
             data = json.load(fp)
-
-        #save a copy of the input config file before update, rename
-        config_keep = "config_" + cat_id + "." + "json"
-        with open(config_keep, 'w') as f:  # writing JSON object
-            json.dump(data, f)
-
-        #new_param = {"maxsmc": 0.439, "satdk":0.00000338, "refkdt":3.0, "slope":0.01, "bb":4.05, "multiplier":100.0, "expon":6.0}
-        new_param = param_dict
-
-        # update calibration parameters in data in json format
-        # during the calibration iteration process, the dataframe file is updated each iteration step
-        # and returned to be used for next iteration as input parameters
-        #for id, params in data['catchments']['cat-87'].items():
-        for id, params in data['catchments'][cat_id].items():
-            print("id, params = {}, {}\n".format(id,params))
-            for param in params:
-                param['init'] = new_param[param['param']]
+            #params, i.e. {"maxsmc": 0.439, "satdk":0.00000338, "refkdt":3.0, "slope":0.01, "bb":4.05, "multiplier":100.0, "expon":6.0}
+            # update calibration parameters in data in json format
+            for param, value in params.set_index('param')[str(i)].iteritems():
+                #TODO/FIXME consider conveying which formulaiton in a meaningful way
+                #For now, update all formulations which contain param
+                for f in data['catchments'][id]['formulations']:
+                    if param in f['params'].keys():
+                        f['params'][param] = value
 
         # write to a json file
-        #config_out = "config_out"
-        #config_out = config_out + "_" + str(i) + "." + "json"
-        #with open('config_out.json', 'w') as f:
-        config_out = config_file
-        with open(config_out, 'w') as f:
-            json.dump(data, f)
+        with open(self._config.config_file, 'w') as fp:
+            json.dump(data, fp, indent=4)
 
 
     @property
