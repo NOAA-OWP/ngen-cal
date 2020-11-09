66eb674b40134fbe14bbe6f676ee4e0be429812a
Nels Frazier <nels.frazier@noaa.gov>
2020-11-09 15:44:45 -0600

    read hydrofabric in confuration init

--- python/ngen_cal/configuration.py
+++ python/ngen_cal/configuration.py
@@ -1,4 +1,9 @@
+import logging
+#supress geopandas debug logs
+logging.disable(logging.DEBUG)
 import json
+import geopandas as gpd
+import pandas as pd
 import shutil
 from pathlib import Path
 from typing import Sequence, TYPE_CHECKING
@@ -20,7 +25,7 @@ class Configuration:
 
     """
 
-    def __init__(self, config_file: 'Path'):
+    def __init__(self, config_file: 'Path', catchment_data: 'Path', nexus_data: 'Path', cross_walk: 'Path', workdir: 'Path'):
         """
 
         """
@@ -28,13 +33,39 @@ class Configuration:
         self._config_file_path = config_file
         #Make a copy of the config file, just in case
         shutil.copy(config_file, str(config_file)+'_original')
+
+        #Read the catchment hydrofabric data
+        catchment_hydro_fabric = gpd.read_file(catchment_data)
+        catchment_hydro_fabric.set_index('ID', inplace=True)
+        nexus_hydro_fabric = gpd.read_file(nexus_data)
+        nexus_hydro_fabric.set_index('ID', inplace=True)
+
+        x_walk = pd.read_json(cross_walk, dtype=str)
+
+        #Read the calibration specific info
         with open(config_file) as fp:
             data = json.load(fp)
-        #Read the calibration specific info
-
+        try:
+            start_t = data['time']['start_time']
+            end_t = data['time']['end_time']
+        except KeyError as e:
+            raise(RuntimeError("Invalid time configuration: {} key missing from {}".format(e.args[0], config_file)))
+        #Setup each calibration catchment
         for id, catchment in data['catchments'].items():
             if 'calibration' in catchment.keys():
-                self._catchments.append(CalibrationCatchment(id, catchment))
+                try:
+                    fabric = catchment_hydro_fabric.loc[id]
+                except KeyError:
+                    #TODO log WARNING:
+                    continue
+                try:
+                    nwis = x_walk[id]['site_no']
+                except KeyError:
+                    raise(RuntimeError("Cannot establish mapping of catchment {} to nwis location in cross walk".format(id)))
+                try:
+                    nexus_data = nexus_hydro_fabric.loc[fabric['toID']]
+                except KeyError:
+                    raise(RuntimeError("No suitable nexus found for catchment {}".format(id)))
 
     @property
     def config_file(self) -> Path:
