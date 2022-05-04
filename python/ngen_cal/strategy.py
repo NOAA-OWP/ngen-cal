b098780826693f35aef1062329815b2c0454400a
Nels Frazier <nels.frazier@noaa.gov>
2022-05-04 14:55:20 -0500

    add objective function mapping for user input

--- python/ngen_cal/strategy.py
+++ python/ngen_cal/strategy.py
@@ -1,10 +1,12 @@
-from pydantic import BaseModel
-from typing import Optional
+from pydantic import BaseModel, PyObject, validator
+from typing import Optional, Union
 try: #to get literal in python 3.7, it was added to typing in 3.8
     from typing import Literal
 except ImportError:
     from typing_extensions import Literal
 from enum import Enum
+
+from . import objectives
 
 class Algorithm(str, Enum):
     """Enumeration of supported search algorithms
@@ -13,6 +15,28 @@ class Algorithm(str, Enum):
     """Dynamic Dimensioned Search Algorithm
     """
     dds = "dds"
+
+class Objective(Enum):
+    """Enumeration of supported search algorithms
+
+    """
+    """Dynamic Dimensioned Search Algorithm
+    """
+    __func_map__ = {"custom": objectives.custom, 
+                    "kling_gupta": objectives.kling_gupta_efficiency,
+                    "nnse": objectives.normalized_nash_sutcliffe,
+                    "single_peak": objectives.peak_error_single,
+                    "volume": objectives.volume_error
+                }
+
+    custom = "custom"
+    kling_gupta = "kling_gupta"
+    nnse = "nnse"
+    single_peak = "single_peak"
+    volume = "volume"
+
+    def __call__(self, *args, **kwargs):
+        return self.__func_map__[self.value](*args, **kwargs)
 
 class Estimation(BaseModel):
     """
@@ -31,10 +55,17 @@ class Estimation(BaseModel):
 
     """
         Optional objective function selector
-
-        TODO make this an enum
+        TODO allow for additional kwargs to be supplied to these functions?
+        Document that all functions must take obs, sim args
     """
-    objective: Optional[str]
+    objective: Optional[Union[Objective, PyObject]] = Objective.custom
+
+    @validator("objective")
+    def validate_objective(cls, value):
+        if value is None:
+            raise ValueError("Objective function must not be None")
+        
+        return value
 
 class Sensitivity(BaseModel):
     """
