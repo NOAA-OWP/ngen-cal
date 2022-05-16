28d2aba4feb4531149e9eee34eb781523511696a
Nels Frazier <nels.frazier@noaa.gov>
2022-05-16 15:37:07 -0500

    add models for complete realizations

---
+++ python/ngen_conf/src/ngen/config/realization.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, Field
+from typing import Optional, Mapping, Sequence
+from datetime import datetime
+from .configurations import Forcing, Time, Routing
+from .formulation import Formulation
+
+class Realization(BaseModel):
+    """Simple model of a Realization, containing formulations and forcing
+    """
+    formulations: Sequence[Formulation]
+    forcing: Forcing
+
+
+
+class NgenRealization(BaseModel):
+    """A complete ngen realization confiiguration model, including global and catchment overrides
+    """
+    global_config: Realization = Field(alias='global')
+    time: Time
+    routing: Optional[Routing]
+    #FIXME have not tested catchments...
+    catchments: Optional[ Mapping[str, Realization] ]
+
+    #FIXME https://github.com/samuelcolvin/pydantic/issues/2277
+    #Until 1.10, it looks like nested encoder config doesn't apply
+    #so you have to define the encoder at the top level object that
+    #will be serialized...
+    class Config:
+        allow_population_by_field_name = True
+        json_encoders = {
+            datetime: lambda v: v.strftime("%Y-%m-%d %H:%M:%S")
+        }
