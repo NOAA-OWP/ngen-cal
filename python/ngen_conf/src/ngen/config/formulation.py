9c63f2f66c10f5617a413ed5bc5cbfdb269276e8
Nels Frazier <nels.frazier@noaa.gov>
2022-05-16 15:35:14 -0500

    add multi-bmi model and Formulation model

---
+++ python/ngen_conf/src/ngen/config/formulation.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field
+
+class Formulation(BaseModel, smart_union=True):
+    """Model of an ngen formulation
+    """
+    #TODO make this an enum?
+    name: str
+    params:  "KnownFormulations" = Field(descriminator="model_name")
+
+#NOTE To avoid circular import and support recrusive modules
+#note that `params` is one of KnownFormulations, 
+#of which MultiBMI may be one of those.  
+#A MultiBMI has a sequence of Formulation objects, making a recursive type
+# So we defer type cheking and importing the KnownFormulations until after
+#MultiBMI is defined, then update_forward_refs()
+from .all_formulations import KnownFormulations
+Formulation.update_forward_refs()
