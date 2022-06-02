f8761ae45a7a9e8d7d50b95aa1c00bd306487ec0
Nels Frazier <nels.frazier@noaa.gov>
2022-06-02 13:18:15 -0500

    monkey patch pydantic for schema generation

--- python/ngen_cal/__init__.py
+++ python/ngen_cal/__init__.py
@@ -1,3 +1,10 @@
+# Monkey patch pydantic to allow schema serialization of PyObject types to string
+from pydantic import PyObject
+def pyobject_schema(cls, field_schema):
+            field_schema['type'] = 'string'
+
+PyObject.__modify_schema__ = classmethod(pyobject_schema)
+
 from .configuration import General, Model
 from .calibratable import Calibratable, Adjustable, Evaluatable
 from .calibration_set import CalibrationSet, UniformCalibrationSet
