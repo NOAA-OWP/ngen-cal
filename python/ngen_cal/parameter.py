80750876f69ef092a52fbff115d99d5963775d34
Nels Frazier <nels.frazier@noaa.gov>
2022-06-02 12:31:52 -0500

    add alias to parammeter name

--- python/ngen_cal/parameter.py
+++ python/ngen_cal/parameter.py
@@ -1,12 +1,12 @@
 from ast import Param
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Sequence
 
-class Parameter(BaseModel):
+class Parameter(BaseModel, allow_population_by_field_name = True):
     """
         The data class for a given parameter
     """
-    name: str
+    name: str = Field(alias='param')
     min: float
     max: float
     init: float
