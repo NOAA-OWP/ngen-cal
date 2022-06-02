673ecbce08cfdabe8227e1953cd72e644b2493c3
Nels Frazier <nels.frazier@noaa.gov>
2022-06-02 13:17:54 -0500

    update package exports

--- python/ngen_cal/__init__.py
+++ python/ngen_cal/__init__.py
@@ -1,4 +1,5 @@
 from .configuration import General, Model
-from .calibratable import Calibratable
+from .calibratable import Calibratable, Adjustable, Evaluatable
+from .calibration_set import CalibrationSet, UniformCalibrationSet
 from .meta import CalibrationMeta
 from .plot import *
