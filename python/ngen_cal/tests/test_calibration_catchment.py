ec6a647b1012f090f2c90d289fb03a10ac15b9a6
Nels Frazier <nels.frazier@noaa.gov>
2022-11-23 09:51:42 -0600

    rework output monkey patch for output test

--- python/ngen_cal/tests/test_calibration_catchment.py
+++ python/ngen_cal/tests/test_calibration_catchment.py
@@ -19,12 +19,12 @@ def test_df(catchment: 'CalibrationCatchment') -> None:
     assert catchment.df.iloc[0]['max'] == 1.0
 
 @pytest.mark.usefixtures("catchment2")
-def test_output(catchment2: 'CalibrationCatchment', mocker) -> None:
+def test_output(catchment2: 'CalibrationCatchment', monkeypatch) -> None:
     """
         Test proper handling of non-existent output
     """
-    mock = mocker.patch('builtins.open')
-    mock.mock_open.side_effect = FileNotFoundError()
+    import pandas as pd
+    monkeypatch.setattr(pd, "read_csv", lambda *args, **kwargs: FileNotFoundError())
     output = catchment2.output
     assert output == None
 
