798c2d9048597ea8d2a83474d83350061b4ba0d7
Nels Frazier <nels.frazier@noaa.gov>
2022-12-08 17:53:08 -0600

    update entrypoint with support for PSO optmizization

--- python/calibration.py
+++ python/calibration.py
@@ -2,11 +2,12 @@
 import yaml
 from os import chdir
 from pathlib import Path
-from ngen.cal.configuration import General, Model
-from ngen.cal.meta import JobMeta
-from ngen.cal.search import dds, dds_set
+from ngen.cal.configuration import General
+from ngen.cal.search import dds, dds_set, pso_search
+from ngen.cal.strategy import Algorithm
+from ngen.cal.agent import Agent
 
-def main(general: General, model: Model):
+def main(general: General, model_conf):
     #seed the random number generators if requested
     if( general.random_seed is not None):
         import random
@@ -20,31 +21,42 @@ def main(general: General, model: Model):
     TODO calibrate each "catcment" independely, but there may be something interesting in grouping various formulation params
     into a single variable vector and calibrating a set of heterogenous formultions...
     """
-
-    meta = JobMeta(model, general)
-    start_iteration = general.start_iteration
-    if general.restart:
-        start_iteration = meta.restart()
-
+    start_iteration = 0
+    #Initialize the starting agent
+    agent = Agent(model_conf, general.workdir, general.log, general.restart, general.strategy.parameters)
+    if(general.strategy.algorithm == Algorithm.dds):
+        func = dds_set #FIXME what about explicit/dds
+        start_iteration = general.start_iteration
+        if general.restart:
+            start_iteration = agent.restart()
+    elif( general.strategy.algorithm == Algorithm.pso): #TODO how to restart PSO?
+        if agent.model.strategy != "uniform":
+            print("Can only use PSO with the uniform model strategy")
+            return
+        if general.restart:
+            print("Restart not supported for PSO search, starting at 0")
+        func = pso_search
 
     print("Starting Iteration: {}".format(start_iteration))
-    print("Starting Best param: {}".format(meta.best_params))
-    print("Starting Best score: {}".format(meta.best_score))
-    print("Starting DDS loop")
+    # print("Starting Best param: {}".format(meta.best_params))
+    # print("Starting Best score: {}".format(meta.best_score))
+    print("Starting calibration loop")
+
 
     #NOTE this assumes we calibrate each catchment independently, it may be possible to design an "aggregate" calibration
     #that works in a more sophisticated manner.
-    if model.strategy == 'explicit':
-        for catchment in model.hy_catchments:
-            dds(start_iteration, general.iterations, catchment, meta)
+    if agent.model.strategy == 'explicit': #FIXME this needs a refactor...should be able to use a calibration_set with explicit loading
+        for catchment in agent.model.adjustables:
+            dds(start_iteration, general.iterations, catchment, agent)
 
-    elif model.strategy == 'independent':
-        for catchment_set in model.hy_catchments:
-            dds_set(start_iteration, general.iterations, catchment_set, meta)
+    elif agent.model.strategy == 'independent':
+        #for catchment_set in agent.model.adjustables:
+        func(start_iteration, general.iterations, agent)
 
-    elif model.strategy == 'uniform':
-        for catchment_set in model.hy_catchments:
-            dds_set(start_iteration, general.iterations, catchment_set, meta)
+    elif agent.model.strategy == 'uniform':
+        #for catchment_set in agent.model.adjustables:
+        #    func(start_iteration, general.iterations, catchment_set, agent)
+        func(start_iteration, general.iterations, agent)
 
 if __name__ == "__main__":
 
@@ -66,5 +78,5 @@ if __name__ == "__main__":
     # change directory to workdir
     chdir(general.workdir)
 
-    model = Model(model=conf['model']).model
-    main(general, model)
+    #model = Model(model=conf['model']).model
+    main(general, conf['model'])
