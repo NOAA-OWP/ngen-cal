f3e87f5600fc0c3a058eb410d9ab37b75dd016fb
Austin Raney <austin.raney@noaa.gov>
2023-03-02 12:26:56 -0500

    add utility validators and root validators

---
+++ python/ngen_init_config/src/ngen/init_config/validators.py
@@ -0,0 +1,12 @@
+from typing import Callable, List
+
+
+def str_split(sep: str, *, strip: bool = False) -> Callable[[str], List[str]]:
+    def inner(items: str) -> List[str]:
+        split_items = items.split(sep)
+        if strip:
+            for idx, item in enumerate(split_items):
+                split_items[idx] = item.strip()
+        return split_items
+
+    return inner
