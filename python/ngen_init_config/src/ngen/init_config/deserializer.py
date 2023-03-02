81f61e1f3359bae6a0d7dc9aa01ae6d7d56c8f5b
Austin Raney <austin.raney@noaa.gov>
2023-03-02 12:25:02 -0500

    add fmt specific de/serialization interface implementations

---
+++ python/ngen_init_config/src/ngen/init_config/deserializer.py
@@ -0,0 +1,89 @@
+from pathlib import Path
+
+from .core import Base
+from .utils import merge_class_attr
+from ._deserializers import (
+    from_ini_str,
+    from_ini_no_section_header_str,
+    from_namelist_str,
+    from_yaml_str,
+    from_toml_str,
+)
+
+from typing_extensions import Self
+
+
+class IniDeserializer(Base):
+    """Blanket implementation for deserializing from ini format. Python's standard library
+    `configparser` package is used to handle deserialization.
+
+    Subtype specific configuration options:
+        Optionally provide configuration options in `Config` class of types that inherits from
+        `IniDeserializer`.
+
+        - `no_section_headers`: bool
+            Denotes that ini file does not have section headers (default: `False`)
+    """
+
+    class Config(Base.Config):
+        no_section_headers: bool = False
+
+    @classmethod
+    def from_ini(cls, p: Path) -> Self:
+        no_section_headers = merge_class_attr(cls, "Config.no_section_headers", False)
+        if no_section_headers:
+            return from_ini_no_section_header_str(p.read_text(), cls)
+        return from_ini_str(p.read_text(), cls)
+
+    @classmethod
+    def from_ini_str(cls, s: str) -> Self:
+        no_section_headers = merge_class_attr(cls, "Config.no_section_headers", False)
+        if no_section_headers:
+            return from_ini_no_section_header_str(s, cls)
+        return from_ini_str(s, cls)
+
+
+class NamelistDeserializer(Base):
+    """Blanket implementation for deserializing from FORTRAN namelist format. The `f90nml` package
+    is used to handle deserialization. `f90nml` is not included in default installations of
+    `ngen.init_config`. Install `ngen.init_config` with `f90nml` using the extra install option,
+    `namelist`.
+    """
+
+    @classmethod
+    def from_namelist(cls, p: Path) -> Self:
+        return from_namelist_str(p.read_text(), cls)
+
+    @classmethod
+    def from_namelist_str(cls, s: str) -> Self:
+        return from_namelist_str(s, cls)
+
+
+class YamlDeserializer(Base):
+    """Blanket implementation for deserializing from yaml format. The `PyYAML` package is used to
+    handle deserialization. `PyYAML` is not included in default installations of `ngen.init_config`.
+    Install `ngen.init_config` with `PyYAML` using the extra install option, `yaml`.
+    """
+
+    @classmethod
+    def from_yaml(cls, p: Path) -> Self:
+        return from_yaml_str(p.read_text(), cls)
+
+    @classmethod
+    def from_yaml_str(cls, s: str) -> Self:
+        return from_yaml_str(s, cls)
+
+
+class TomlDeserializer(Base):
+    """Blanket implementation for deserializing from `toml` format. The `tomli` package is used to
+    handle deserialization. `tomli` is not included in default installations of `ngen.init_config`.
+    Install `ngen.init_config` with `tomli` using the extra install option, `toml`.
+    """
+
+    @classmethod
+    def from_toml(cls, p: Path) -> Self:
+        return from_toml_str(p.read_text(), cls)
+
+    @classmethod
+    def from_toml_str(cls, s: str) -> Self:
+        return from_toml_str(s, cls)
---
+++ python/ngen_init_config/src/ngen/init_config/serializer.py
@@ -0,0 +1,111 @@
+from pathlib import Path
+
+from .core import Base
+from .utils import merge_class_attr
+from ._serlializers import (
+    to_namelist_str,
+    to_ini_str,
+    to_ini_no_section_header_str,
+    to_yaml_str,
+    to_toml_str,
+)
+
+
+class IniSerializer(Base):
+    """Blanket implementation for serializing into ini format. Python's standard library
+    `configparser` package is used to handle serialization.
+
+    Fields are serialized using their alias, if provided.
+
+    Subtype specific configuration options:
+        Optionally provide configuration options in `Config` class of types that inherits from
+        `IniSerializer`.
+
+        - `no_section_headers`: bool
+            If True, output ini will not have section headers (default: `False`)
+        - `space_around_delimiters`: bool
+            If True, delimiters between keys and values are surrounded by spaces (default: `True`)
+    """
+
+    class Config(Base.Config):
+        no_section_headers: bool = False
+        space_around_delimiters: bool = True
+
+    def to_ini(self, p: Path) -> None:
+        with open(p, "w") as f:
+            if self._no_section_headers:
+                f.write(
+                    to_ini_no_section_header_str(
+                        self, space_around_delimiters=self._space_around_delimiters
+                    )
+                )
+                return
+
+            f.write(
+                to_ini_str(self, space_around_delimiters=self._space_around_delimiters)
+            )
+
+    def to_ini_str(self) -> str:
+        if self._no_section_headers:
+            return to_ini_no_section_header_str(
+                self, space_around_delimiters=self._space_around_delimiters
+            )
+
+        return to_ini_str(self, space_around_delimiters=self._space_around_delimiters)
+
+    @property
+    def _space_around_delimiters(self) -> bool:
+        return merge_class_attr(type(self), "Config.space_around_delimiters", True)  # type: ignore
+
+    @property
+    def _no_section_headers(self) -> bool:
+        return merge_class_attr(type(self), "Config.no_section_headers", False)  # type: ignore
+
+
+class NamelistSerializer(Base):
+    """Blanket implementation for serializing into FORTRAN namelist format. The `f90nml` package is
+    used to handle serialization. `f90nml` is not included in default installations of
+    `ngen.init_config`. Install `ngen.init_config` with `f90nml` using the extra install option,
+    `namelist`.
+
+    Fields are serialized using their alias, if provided.
+    """
+
+    def to_namelist(self, p: Path) -> None:
+        with open(p, "w") as f:
+            f.write(to_namelist_str(self))
+
+    def to_namelist_str(self) -> str:
+        return to_namelist_str(self)
+
+
+class YamlSerializer(Base):
+    """Blanket implementation for serializing from yaml format. The `PyYAML` package is used to
+    handle serialization. `PyYAML` is not included in default installations of `ngen.init_config`.
+    Install `ngen.init_config` with `PyYAML` using the extra install option, `yaml`.
+
+    Fields are serialized using their alias, if provided.
+    """
+
+    def to_yaml(self, p: Path) -> None:
+        with open(p, "w") as f:
+            f.write(to_yaml_str(self))
+
+    def to_yaml_str(self) -> str:
+        return to_yaml_str(self)
+
+
+class TomlSerializer(Base):
+    """Blanket implementation for serializing from `toml` format. The `tomli_w` package is used to
+    handle serialization. `tomli_w` is not included in default installations of `ngen.init_config`.
+    Install `ngen.init_config` with `tomli_w` using the extra install option, `toml`.
+
+    Fields are serialized using their alias, if provided.
+    """
+
+    def to_toml(self, p: Path) -> None:
+        with open(p, "w") as f:
+            f.write(to_toml_str(self))
+
+    def to_toml_str(self) -> str:
+        return to_toml_str(self)
