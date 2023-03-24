fb2ea0894e398c0c08a1b9e403e512421c54e11f
Austin Raney <austin.raney@noaa.gov>
2023-03-24 14:22:43 -0400

    add PET default registration_function. fixes #45.

--- python/ngen_conf/src/ngen/config/pet.py
+++ python/ngen_conf/src/ngen/config/pet.py
@@ -9,6 +9,8 @@ class PET(BMIC):
     main_output_variable: Literal["water_potential_evaporation_flux"] = "water_potential_evaporation_flux"
     #NOTE aliases don't propagate to subclasses, so we have to repeat the alias
     model_name: str = Field("PET", alias="model_type_name")
+    # source: https://github.com/NOAA-OWP/evapotranspiration/blob/0a66999db9695bccf4c1e35d904aa86f04e6cacf/src/bmi_pet.c#L1215
+    registration_function: str = "register_bmi_pet"
 
     _variable_names_map =  {
             "water_potential_evaporation_flux":"water_potential_evaporation_flux"
