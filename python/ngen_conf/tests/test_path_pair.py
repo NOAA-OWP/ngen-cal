fb56b9f427d292f00b695380c97adebf39459bff
Austin Raney <austin.raney@noaa.gov>
2023-03-30 09:46:53 -0400

    add PathPairCollection tests. still have more to write

--- python/ngen_conf/tests/test_path_pair.py
+++ python/ngen_conf/tests/test_path_pair.py
@@ -97,3 +97,103 @@ def test_path_read(path_pair: PosixPathPair[InnerModel]):
         assert p.exists()
         assert o2.read() == True
         assert o2.inner == o.inner == path_pair.inner
+
+
+@pytest.fixture
+def collection() -> PosixPathPairCollection[InnerModel]:
+    """Does not try to clean up and created assets."""
+    p = Path("id_{id}_data.txt")
+    pattern = "{id}"
+    models = [InnerModel(foo=i) for i in range(12)]
+    ids = list(map(lambda s: str(s), range(12)))
+
+    return PathPairCollection.from_objects(
+        models,
+        pattern=pattern,
+        path=p,
+        ids=ids,
+        serializer=pydantic_serializer,
+        deserializer=pydantic_deserializer(InnerModel),
+    )
+
+
+@pytest.fixture
+def temp_collection(
+    collection: PosixPathPairCollection[InnerModel],
+) -> PosixPathPairCollection[InnerModel]:
+    """
+    `PathPairCollection` backed by a temp directory. This guarantees that files / dirs created in the
+    temp dir will be cleaned up once the test using this fixture has run.
+
+    Only use this if you need to test writing / reading.
+    """
+    with tempfile.TemporaryDirectory() as dir:
+        o = Path(dir) / collection
+        yield o
+        o.unlink(missing_ok=True)
+
+
+def test_path_pair_collection_write(
+    temp_collection: PosixPathPairCollection[InnerModel],
+):
+    prefix, _, suffix = temp_collection.name.partition(temp_collection.pattern)
+    glob_term = f"{prefix}*{suffix}"
+
+    # assert no files have been written
+    assert len(list(temp_collection.parent.glob(glob_term))) == 0
+
+    temp_collection.write()
+
+    # assert all files have been written
+    assert len(list(temp_collection.parent.glob(glob_term))) == len(
+        list(temp_collection.inner)
+    )
+
+
+def test_path_pair_collection_read(
+    temp_collection: PosixPathPairCollection[InnerModel],
+):
+    prefix, _, suffix = temp_collection.name.partition(temp_collection.pattern)
+    glob_term = f"{prefix}*{suffix}"
+
+    assert len(list(temp_collection.parent.glob(glob_term))) == 0
+
+    temp_collection.write()
+
+    c2 = PathPairCollection(
+        temp_collection,
+        pattern=temp_collection.pattern,
+        deserializer=temp_collection._deserializer,
+    )
+
+    assert c2.read() == True
+
+    # sort inner list[ObjectPosixPathPair] b.c. _get_filenames uses `glob` which is not ordered
+    temp_collection._inner.sort()
+    c2._inner.sort()
+
+    # this compares Path values, not inner
+    assert temp_collection._inner == c2._inner
+
+    for col_item, c2_item in zip(temp_collection._inner, c2._inner):
+        assert col_item.inner == c2_item.inner
+
+
+def test_path_pair_collection_truediv_is_noop(
+    collection: PosixPathPairCollection[InnerModel],
+):
+    o = collection / "test"
+    assert o == collection
+
+
+def test_path_pair_collection_rtruediv(
+    collection: PosixPathPairCollection[InnerModel],
+):
+    new_root = Path("new/root")
+    o = new_root / collection
+
+    assert o == new_root / collection.name
+    assert type(o) == type(collection)
+    for prev_pair, new_pair in zip(collection.inner_pair, o.inner_pair):
+        assert prev_pair.name == new_pair.name
+        assert new_pair.parent == new_root
