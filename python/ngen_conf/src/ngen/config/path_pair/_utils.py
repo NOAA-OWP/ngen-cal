7cbb424b2b2690f420614caadfeade10099f93df
Austin Raney <austin.raney@noaa.gov>
2023-03-30 14:46:04 -0400

    add 3.7 Path.unlink missing_ok arg work around
    see: https://docs.python.org/3.8/library/pathlib.html#pathlib.Path.unlink
    > _Changed_ in version 3.8: The _missing_ok_ parameter was added.
    TODO: remove once we drop 3.7 support

---
+++ python/ngen_conf/src/ngen/config/path_pair/_utils.py
@@ -0,0 +1,13 @@
+import sys
+from pathlib import Path
+
+
+def path_unlink_37(p: Path, missing_ok: bool):
+    # see: https://docs.python.org/3.8/library/pathlib.html#pathlib.Path.unlink
+    # > _Changed_ in version 3.8: The _missing_ok_ parameter was added.
+    # TODO: remove once we drop 3.7 support
+    if sys.version_info >= (3, 8):
+        p.unlink(missing_ok=missing_ok)
+    if missing_ok and not p.exists():
+        return
+    p.unlink()
