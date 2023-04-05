b710bc17564af7b8db2d4fc4e19f77ff061baa24
Austin Raney <austin.raney@noaa.gov>
2023-04-05 16:08:33 -0400

    test PET init config model

--- python/ngen_conf/tests/test_init_config_models.py
+++ python/ngen_conf/tests/test_init_config_models.py
@@ -1,5 +1,6 @@
 from ngen.init_config import utils
 from ngen.config.init_config.cfe import CFE
+from ngen.config.init_config.pet import PET
 
 
 def test_cfe(cfe_init_config: str):
@@ -7,3 +8,10 @@ def test_cfe(cfe_init_config: str):
     assert utils.merge_class_attr(CFE, "Config.no_section_headers") is True
     o = CFE.from_ini_str(cfe_init_config)
     assert o.to_ini_str() == cfe_init_config
+
+
+def test_pet(pet_init_config: str):
+    assert utils.merge_class_attr(PET, "Config.space_around_delimiters") is False
+    assert utils.merge_class_attr(PET, "Config.no_section_headers") is True
+    o = PET.from_ini_str(pet_init_config)
+    assert o.to_ini_str() == pet_init_config
