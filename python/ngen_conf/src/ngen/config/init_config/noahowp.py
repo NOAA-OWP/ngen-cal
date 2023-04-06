27828b76a91e257130e51a97091cb1a5604f5346
Austin Raney <austin.raney@noaa.gov>
2023-04-06 12:11:26 -0400

    add Noah OWP init config

---
+++ python/ngen_conf/src/ngen/config/init_config/noahowp.py
@@ -0,0 +1,248 @@
+from enum import Enum
+from datetime import datetime
+from pathlib import Path, PosixPath, WindowsPath
+from pydantic import validator
+
+from ngen.init_config import core
+from ngen.init_config import serializer_deserializer as serde
+
+from .noahowp_options import (
+    PrecipPhaseOption,
+    SnowAlbedoOption,
+    DynamicVegOption,
+    RunoffOption,
+    DrainageOption,
+    FrozenSoilOption,
+    DynamicVicOption,
+    RadiativeTransferOption,
+    SfcDragCoeffOption,
+    CanopyStomResistOption,
+    CropModelOption,
+    SnowsoilTempTimeOption,
+    SoilTempBoundaryOption,
+    SupercooledWaterOption,
+    StomatalResistanceOption,
+    EvapSrfcResistanceOption,
+    SubsurfaceOption,
+)
+from .validators import validate_str_len_lt
+from .utils import serialize_enum_value
+
+from typing import ClassVar, List, Literal, Union
+
+
+class NoahOWP(serde.NamelistSerializerDeserializer):
+    timing: "Timing"
+    parameters: "Parameters"
+    location: "Location"
+    forcing: "Forcing"
+    model_options: "ModelOptions"
+    structure: "Structure"
+    initial_values: "InitialValues"
+
+    class Config(serde.NamelistSerializerDeserializer.Config):
+        # NOTE: must explicitly specify Path subtype (i.e. `PosixPath`) here b.c.
+        # `field_type_serializers` does an explicit type check, not a subtypes (covariant)
+        # check.
+        field_type_serializers = {
+            # serialize Path types using a str cast. this ensures paths are properly escaped.
+            PosixPath: lambda p: str(p),
+            WindowsPath: lambda p: str(p),
+        }
+
+
+class Timing(core.Base):
+    dt: float
+    startdate: datetime
+    enddate: datetime
+    forcing_filename: Path
+    output_filename: Path
+
+    # format: YYYYMMDDHHmm (e.g. 199801010630)
+    # source: https://github.com/NOAA-OWP/noah-owp-modular/blob/30d0f53e8c14acc4ce74018e06ff7c9410ecc13c/src/DomainType.f90#L15
+    _datetime_format: ClassVar[str] = "%Y%m%d%H%M"
+
+    _validate_str_len = validator(
+        "forcing_filename", "output_filename", pre=True, allow_reuse=True
+    )(validate_str_len_lt(257))
+
+    @validator("startdate", "enddate", pre=True)
+    def _validate_dates(cls, value: Union[datetime, str]):
+        if isinstance(value, datetime):
+            return value
+        return datetime.strptime(value, cls._datetime_format)
+
+    class Config(core.Base.Config):
+        field_serializers = {
+            "startdate": lambda d: d.strftime(Timing._datetime_format),
+            "enddate": lambda d: d.strftime(Timing._datetime_format),
+        }
+
+
+class Parameters(core.Base):
+    parameter_dir: Path
+    general_table: str = "GENPARM.TBL"
+    soil_table: str = "SOILPARM.TBL"
+    noahowp_table: str = "MPTABLE.TBL"
+    soil_class_name: Literal["STAS", "STAS-RUC"]
+    veg_class_name: Literal["MODIFIED_IGBP_MODIS_NOAH", "USGS"]
+
+    _validate_str_len = validator(
+        "parameter_dir",
+        "general_table",
+        "soil_table",
+        "noahowp_table",
+        pre=True,
+        allow_reuse=True,
+    )(validate_str_len_lt(257))
+
+    class Config(core.Base.Config):
+        def _serialize_parameter_dir(p: Path) -> str:
+            if isinstance(p, WindowsPath):
+                return rf"{str(p)}\\"
+            return f"{str(p)}/"
+
+        # `parameter_dir` should end in platform specific '/'
+        field_serializers = {"parameter_dir": _serialize_parameter_dir}
+        fields = {
+            "parameter_dir": {"description": "directory path containing table files"},
+            "general_table": {"description": "general param tables and misc params"},
+            "soil_table": {"description": "soil param table"},
+            "noahowp_table": {"description": " noah-mp related param tables"},
+            "soil_class_name": {"description": "soil class data source"},
+            "veg_class_name": {"description": "vegetation class data source"},
+        }
+
+
+class Location(core.Base):
+    lat: float
+    lon: float
+    terrain_slope: float
+    azimuth: float
+
+    class Config(core.Base.Config):
+        fields = {
+            "lat": {"description": "latitude [degrees]"},
+            "lon": {"description": "longitude [degrees]"},
+            "terrain_slope": {"description": "terrain slope [degrees]"},
+            "azimuth": {
+                "description": "terrain azimuth or aspect [degrees clockwise from north]"
+            },
+        }
+
+
+class Forcing(core.Base):
+    zref: float  #               = 10.0
+    rain_snow_thresh: float  #   = 1.0
+
+    class Config(core.Base.Config):
+        allow_population_by_field_name = True
+        fields = {
+            "zref": {
+                "alias": "ZREF",
+                "description": "measurement height for wind speed [m]",
+            },
+            "rain_snow_thresh": {
+                "description": "rain-snow temperature threshold [degrees C]"
+            },
+        }
+
+
+class ModelOptions(core.Base):
+    # option bounds translated from
+    # https://github.com/NOAA-OWP/noah-owp-modular/blob/30d0f53e8c14acc4ce74018e06ff7c9410ecc13c/src/OptionsType.f90#L11
+    precip_phase_option: PrecipPhaseOption
+    snow_albedo_option: SnowAlbedoOption
+    dynamic_veg_option: DynamicVegOption
+    runoff_option: RunoffOption
+    drainage_option: DrainageOption
+    frozen_soil_option: FrozenSoilOption
+    dynamic_vic_option: DynamicVicOption
+    radiative_transfer_option: RadiativeTransferOption
+    sfc_drag_coeff_option: SfcDragCoeffOption
+    canopy_stom_resist_option: CanopyStomResistOption
+    crop_model_option: CropModelOption
+    snowsoil_temp_time_option: SnowsoilTempTimeOption
+    soil_temp_boundary_option: SoilTempBoundaryOption
+    supercooled_water_option: SupercooledWaterOption
+    stomatal_resistance_option: StomatalResistanceOption
+    evap_srfc_resistance_option: EvapSrfcResistanceOption
+    subsurface_option: SubsurfaceOption
+
+    class Config(core.Base.Config):
+        # TODO: consider serializing all enum's using their value unless otherwise specified
+        field_serializers = {
+            "precip_phase_option": serialize_enum_value,
+            "snow_albedo_option": serialize_enum_value,
+            "dynamic_veg_option": serialize_enum_value,
+            "runoff_option": serialize_enum_value,
+            "drainage_option": serialize_enum_value,
+            "frozen_soil_option": serialize_enum_value,
+            "dynamic_vic_option": serialize_enum_value,
+            "radiative_transfer_option": serialize_enum_value,
+            "sfc_drag_coeff_option": serialize_enum_value,
+            "canopy_stom_resist_option": serialize_enum_value,
+            "crop_model_option": serialize_enum_value,
+            "snowsoil_temp_time_option": serialize_enum_value,
+            "soil_temp_boundary_option": serialize_enum_value,
+            "supercooled_water_option": serialize_enum_value,
+            "stomatal_resistance_option": serialize_enum_value,
+            "evap_srfc_resistance_option": serialize_enum_value,
+            "subsurface_option": serialize_enum_value,
+        }
+
+
+class LandSurfaceType(int, Enum):
+    """
+    land surface type, 1: soil, 2: lake
+    source: https://github.com/NOAA-OWP/noah-owp-modular/blob/30d0f53e8c14acc4ce74018e06ff7c9410ecc13c/src/NamelistRead.f90#L37
+    """
+
+    soil = 1
+    lake = 2
+
+
+class Structure(core.Base):
+    isltyp: int  #    = 1
+    nsoil: int  #     = 4
+    nsnow: int  #     = 3
+    nveg: int  #      = 20
+    vegtyp: int  #    = 1
+    # crop type (SET TO 0, no crops currently supported)
+    # source: https://github.com/NOAA-OWP/noah-owp-modular/blob/30d0f53e8c14acc4ce74018e06ff7c9410ecc13c/src/NamelistRead.f90#L36
+    croptype: int = 0
+    sfctyp: LandSurfaceType  #    = 1
+    soilcolor: int  # = 4
+
+    class Config(core.Base.Config):
+        field_serializers = {"sfctyp": serialize_enum_value}
+        fields = {
+            "isltyp": {"description": "soil texture class"},
+            "nsoil": {"description": "number of soil levels"},
+            "nsnow": {"description": "number of snow levels"},
+            "nveg": {"description": "number of vegetation types"},
+            "vegtyp": {"description": "vegetation type modis"},
+            "croptype": {
+                "description": "crop type (0 = no crops); crop type (SET TO 0, no crops currently supported)"
+            },
+            "sfctyp": {"description": "land surface type, 1:soil, 2:lake"},
+            "soilcolor": {"description": "soil color code"},
+        }
+
+
+class InitialValues(core.Base):
+    dzsnso: List[float]  # =  0.0,  0.0,  0.0,  0.1,  0.3,  0.6,  1.0
+    sice: List[float]  # =  0.0,  0.0,  0.0,  0.0
+    sh2o: List[float]  # =  0.3,  0.3,  0.3,  0.3
+    zwt: float  # =  -2.0
+
+    class Config(core.Base.Config):
+        fields = {
+            "dzsnso": {"description": "snow/soil level thickness [m]"},
+            "sice": {"description": "initial soil ice profile [m^3/m^3]"},
+            "sh2o": {"description": "initial soil liquid profile [m^3/m^3]"},
+            "zwt": {"description": "initial water table depth below surface [m]"},
+        }
+
+
+NoahOWP.update_forward_refs()
---
+++ python/ngen_conf/src/ngen/config/init_config/noahowp_options.py
@@ -0,0 +1,282 @@
+from enum import Enum
+
+
+class PrecipPhaseOption(int, Enum):
+    """
+    integer :: opt_snf    ! precip_phase_option: options for determining precipitation phase
+        1 -> Jordan (1991) SNTHERM equation
+        2 -> rain-snow air temperature threshold of 2.2°C
+        3 -> rain-snow air temperature threshold of 0°C
+        4 -> precipitation phase from weather model
+        5 -> user-defined air temperature threshold
+        6 -> user-defined wet bulb temperature threshold
+        7 -> binary logistic regression model from Jennings et al. (2018)
+    source: https://github.com/NOAA-OWP/noah-owp-modular/blob/30d0f53e8c14acc4ce74018e06ff7c9410ecc13c/src/OptionsType.f90#L11-L18
+    """
+
+    sntherm = 1
+    rain_snow_air_temp_threshold_2_2_C = 2
+    rain_snow_air_temp_threshold_0_C = 3
+    precip_phase_from_weather_model = 4
+    user_defined_air_temp_threshold = 5
+    user_defined_wet_bulb_temperature_threshold = 6
+    binary_logistic_regression_model = 7
+
+
+class RunoffOption(int, Enum):
+    """
+    integer :: opt_run    ! runoff_option: options for runoff
+        1 -> TOPMODEL with groundwater (Niu et al. 2007 JGR)
+        2 -> TOPMODEL with an equilibrium water table (Niu et al. 2005 JGR)
+        3 -> original surface and subsurface runoff (free drainage)
+        4 -> BATS surface and subsurface runoff (free drainage)
+        5 -> Miguez-Macho&Fan groundwater scheme (Miguez-Macho et al. 2007 JGR; Fan et al. 2007 JGR)
+        6 -> VIC runoff
+        7 -> Xinanjiang runoff
+        8 -> Dynamic VIC runoff
+
+    source: https://github.com/NOAA-OWP/noah-owp-modular/blob/30d0f53e8c14acc4ce74018e06ff7c9410ecc13c/src/OptionsType.f90#L19-L27
+    """
+
+    topmodel_with_groundwater = 1
+    topmodel_with_equilibrium_water_table = 2
+    original_surface_and_subsurface_runoff = 3
+    bats_surface_and_subsurface_runoff = 4
+    Miguez_Macho_Fan_groundwater_scheme = 5
+    vic_runoff = 6
+    Xinanjiang_runoff = 7
+    dynamic_vic_runoff = 8
+
+
+class DrainageOption(int, Enum):
+    """
+    integer :: opt_drn    ! drainage_option
+                        ! options for drainage
+        1 -> Subsurface runoff scheme from groundwater module (Niu et al. 2007 JGR)
+        2 -> Subsurface runoff scheme with an equilibrium water table (Niu et al. 2005 JGR)
+        3 -> original subsurface runoff (free drainage)
+        4 -> original subsurface runoff (same as opt_drn = 3 but uses values from opt_run = 4)
+        5 -> drainage with MMF shallow water table scheme
+        6 -> VIC with free drainage (same as opt_drn = 3 but uses values from opt_run = 6)
+        7 -> Xinanjiang runoff (same as opt_drn = 3 but uses values from opt_run = 7)
+        8 -> Dynamic VIC runoff (same as opt_drn = 3 but uses values from opt_run = 8)
+    source: https://github.com/NOAA-OWP/noah-owp-modular/blob/30d0f53e8c14acc4ce74018e06ff7c9410ecc13c/src/OptionsType.f90#L28-L37
+    """
+
+    subsurface_runoff_scheme_from_groundwater_module = 1
+    subsurface_runoff_scheme_with_an_equilibrium_water_table = 2
+    original_subsurface_runoff = 3
+    original_subsurface_runoff_with_bats = 4
+    drainage_with_MMF_shallow_water_table_scheme = 5
+    vic_with_free_drainage_with_vic_runoff = 6
+    Xinanjiang_runoff_with_Xinanjiang_runoff = 7
+    dynamic_vic_runoff_with_dynamic_vic_runoff = 8
+
+
+class FrozenSoilOption(int, Enum):
+    """
+    integer :: opt_inf    ! frozen_soil_option
+                        ! options for frozen soil permeability
+        1 -> linear effects, more permeable (Niu and Yang, 2006, JHM)
+        2 -> nonlinear effects, less permeable (old)
+    source: https://github.com/NOAA-OWP/noah-owp-modular/blob/30d0f53e8c14acc4ce74018e06ff7c9410ecc13c/src/OptionsType.f90#L38-L41
+    """
+
+    linear_effects = 1
+    non_linear_effects = 1
+
+
+class DynamicVicOption(int, Enum):
+    """
+    integer :: opt_infdv  ! dynamic_vic_option
+                        ! options for infiltration in dynamic VIC runoff scheme
+        1 -> Philip scheme,
+        2 -> Green-Ampt scheme
+        3 -> Smith-Parlange scheme
+    source: https://github.com/NOAA-OWP/noah-owp-modular/blob/30d0f53e8c14acc4ce74018e06ff7c9410ecc13c/src/OptionsType.f90#L42-L46
+    """
+
+    Philip = 1
+    Green_Ampt = 2
+    Smith_Parlange = 3
+
+
+class DynamicVegOption(int, Enum):
+    """
+    integer :: dveg       ! dynamic_veg_option
+                        ! options for dynamic vegetation scheme
+        1 -> off (use table LAI; use FVEG = SHDFAC from input)
+        2 -> on  (together with OPT_CRS = 1)
+        3 -> off (use table LAI; calculate FVEG)
+        4 -> off (use table LAI; use maximum vegetation fraction)
+        5 -> on  (use maximum vegetation fraction)
+        6 -> on  (use FVEG = SHDFAC from input)
+        7 -> off (use input LAI; use FVEG = SHDFAC from input)
+        8 -> off (use input LAI; calculate FVEG)
+        9 -> off (use input LAI; use maximum vegetation fraction)
+    source: https://github.com/NOAA-OWP/noah-owp-modular/blob/30d0f53e8c14acc4ce74018e06ff7c9410ecc13c/src/OptionsType.f90#L47-L57
+    """
+
+    off_use_LAI_table_FVEG_eq_SHDFAC_from_input = 1
+    on_with_canopy_stom_resist_option_Ball_Berry = 2
+    off_use_LAI_table_calculate_FVEG = 3
+    off_use_LAI_table_use_max_vegetation_fraction = 4
+    on_use_max_vegetation_fraction = 5
+    on_use_FVEG_eq_SHDFAC_from_input = 6
+    off_use_input_LAI_use_FVEG_eq_SHDFAC_from_input = 7
+    off_use_input_LAI_calculate_FVEG = 8
+    off_use_input_LAI_use_max_vegetation_fraction = 9
+
+
+class SnowAlbedoOption(int, Enum):
+    """
+    integer :: opt_alb    ! snow_albedo_option
+                        ! options for snow albedo
+        1 -> BATS
+        2 -> CLASS
+    source: https://github.com/NOAA-OWP/noah-owp-modular/blob/30d0f53e8c14acc4ce74018e06ff7c9410ecc13c/src/OptionsType.f90#L58-L61
+    """
+
+    BATS = 1
+    CLASS = 2
+
+
+class RadiativeTransferOption(int, Enum):
+    """
+    integer :: opt_rad    ! radiative_transfer_option
+                        ! options for radiative transfer
+        1 -> modified two-stream (gap = F(solar angle, 3D structure ...)<1-FVEG)
+        2 -> two-stream applied to grid-cell (gap = 0)
+        3 -> two-stream applied to vegetated fraction (gap=1-FVEG)
+    source: https://github.com/NOAA-OWP/noah-owp-modular/blob/30d0f53e8c14acc4ce74018e06ff7c9410ecc13c/src/OptionsType.f90#L62-L66
+    """
+
+    modified_two_stream = 1
+    two_stream_applied_to_grid_cell = 2
+    two_stream_applied_to_vegetated_fraction = 3
+
+
+class SfcDragCoeffOption(int, Enum):
+    """
+    integer :: opt_sfc    ! sfc_drag_coeff_option
+                        ! options for surface layer drag coeff (CH & CM)
+        1 -> M-O
+        2 -> original Noah (Chen97)
+    source: https://github.com/NOAA-OWP/noah-owp-modular/blob/30d0f53e8c14acc4ce74018e06ff7c9410ecc13c/src/OptionsType.f90#L67-L70
+    """
+
+    M_O = 1
+    Noah = 2
+
+
+class CanopyStomResistOption(int, Enum):
+    """
+    integer :: opt_crs    ! canopy_stom_resist_option
+                        ! options for canopy stomatal resistance
+        1 -> Ball-Berry
+        2 -> Jarvis
+    source: https://github.com/NOAA-OWP/noah-owp-modular/blob/30d0f53e8c14acc4ce74018e06ff7c9410ecc13c/src/OptionsType.f90#L71-L74
+    """
+
+    Ball_Beryy = 1
+    Jarvis = 2
+
+
+class CropModelOption(int, Enum):
+    """
+    integer :: opt_crop   ! crop_model_option
+                        ! options for crop model
+                        ! NO CROP MODEL CURRENTLY SUPPORTED
+        0 -> No crop model, will run default dynamic vegetation
+    source: https://github.com/NOAA-OWP/noah-owp-modular/blob/30d0f53e8c14acc4ce74018e06ff7c9410ecc13c/src/OptionsType.f90#L75-L78
+    """
+
+    none = 0
+
+
+class SnowsoilTempTimeOption(int, Enum):
+    """
+    integer :: opt_stc    ! snowsoil_temp_time_option
+                        ! options for snow/soil temperature time scheme (only layer 1)
+        1 -> semi-implicit; flux top boundary condition
+        2 -> full implicit (original Noah); temperature top boundary condition
+        3 -> same as 1, but FSNO for TS calculation (generally improves snow; v3.7)
+    source: https://github.com/NOAA-OWP/noah-owp-modular/blob/30d0f53e8c14acc4ce74018e06ff7c9410ecc13c/src/OptionsType.f90#L79-L83
+    """
+
+    semi_implicit = 1
+    full_implicit = 2
+    semo_implicit_with_FSNO_for_TS = 3
+
+
+class SoilTempBoundaryOption(int, Enum):
+    """
+    integer :: opt_tbot   ! soil_temp_boundary_option
+                        ! options for lower boundary condition of soil temperature
+        1 -> zero heat flux from bottom (ZBOT and TBOT not used)
+        2 -> TBOT at ZBOT (8m) read from a file (original Noah)
+    source: https://github.com/NOAA-OWP/noah-owp-modular/blob/30d0f53e8c14acc4ce74018e06ff7c9410ecc13c/src/OptionsType.f90#L84-L87
+    """
+
+    zero_heat_flux_from_bottom = 1
+    TBOT_at_ZBOT = 2
+
+
+class SupercooledWaterOption(int, Enum):
+    """
+    integer :: opt_frz    ! supercooled_water_option
+                        ! options for supercooled liquid water (or ice fraction)
+        1 -> no iteration (Niu and Yang, 2006 JHM)
+        2 -> nonlinear effects, less permeable (old)
+    source: https://github.com/NOAA-OWP/noah-owp-modular/blob/30d0f53e8c14acc4ce74018e06ff7c9410ecc13c/src/OptionsType.f90#L88-L91
+    """
+
+    no_iteration = 1
+    nonlinear = 2
+
+
+class StomatalResistanceOption(int, Enum):
+    """
+    integer :: opt_btr    ! stomatal_resistance_option
+                        ! options for soil moisture factor for stomatal resistance
+        1 -> Noah (soil moisture)
+        2 -> CLM  (matric potential)
+        3 -> SSiB (matric potential)
+    source: https://github.com/NOAA-OWP/noah-owp-modular/blob/30d0f53e8c14acc4ce74018e06ff7c9410ecc13c/src/OptionsType.f90#L92-L96
+    """
+
+    Noah = 1
+    CLM = 2
+    SSiB = 3
+
+
+class EvapSrfcResistanceOption(int, Enum):
+    """
+    integer :: opt_rsf    ! evap_srfc_resistance_option
+                        ! options for surface resistance to evaporation/sublimation
+        1 -> Sakaguchi and Zeng, 2009
+        2 -> Sellers (1992)
+        3 -> adjusted Sellers to decrease RSURF for wet soil
+        4 -> option 1 for non-snow; rsurf = rsurf_snow for snow (set in MPTABLE); AD v3.8
+    source: https://github.com/NOAA-OWP/noah-owp-modular/blob/30d0f53e8c14acc4ce74018e06ff7c9410ecc13c/src/OptionsType.f90#L97-L102
+    """
+
+    Sakaguchi_and_Zeng = 1
+    Sellers = 2
+    adjusted_Sellers = 3
+    Sakaguchi_and_Zeng_for_nonsnow_rsurf_eq_rsurf_snow_for_snow = 4
+
+
+class SubsurfaceOption(int, Enum):
+    """
+    integer :: opt_sub    ! subsurface_option
+                        ! options for subsurface realization
+        1 -> full Noah-MP style subsurface
+        2 -> one-way coupled hydrostatic
+        3 -> two-way coupled (NOT IMPLEMENTED YET)
+    source: https://github.com/NOAA-OWP/noah-owp-modular/blob/30d0f53e8c14acc4ce74018e06ff7c9410ecc13c/src/OptionsType.f90#L103-L107
+    """
+
+    Noah_MP = 1
+    one_way_coupled_hydrostatic = 2
+    two_way_coupled_hydrostatic_NOT_IMPLEMENTED = 2
