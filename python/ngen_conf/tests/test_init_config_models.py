9d1ec66ebafe2f3061de67dc47c8c5c9eee47a8f
Austin Raney <austin.raney@noaa.gov>
2023-04-06 12:12:46 -0400

    add Noah OWP init config test

--- python/ngen_conf/tests/test_init_config_models.py
+++ python/ngen_conf/tests/test_init_config_models.py
@@ -1,6 +1,7 @@
 from ngen.init_config import utils
 from ngen.config.init_config.cfe import CFE
 from ngen.config.init_config.pet import PET
+from ngen.config.init_config.noahowp import NoahOWP
 
 
 def test_cfe(cfe_init_config: str):
@@ -15,3 +16,8 @@ def test_pet(pet_init_config: str):
     assert utils.merge_class_attr(PET, "Config.no_section_headers") is True
     o = PET.from_ini_str(pet_init_config)
     assert o.to_ini_str() == pet_init_config
+
+
+def test_noah_owp(noah_owp_init_config: str):
+    o = NoahOWP.from_namelist_str(noah_owp_init_config)
+    assert o.to_namelist_str() == noah_owp_init_config
