a9e709bd6ac2aa4622f07934298d4857569303d8
Jordan Laser <jordan.science.laser@gmail.com>
2023-04-07 10:26:29 -0500

    added fixture for testdir and swapped model names

--- python/ngen_conf/tests/test_conf.py
+++ python/ngen_conf/tests/test_conf.py
@@ -1,38 +1,40 @@
 import pytest, json
 from pathlib import Path
 from ngen.config.realization import NgenRealization
-from ngen.config.hydrofabric import NGenNexusFile, NGenCatchmentFile
+from ngen.config.hydrofabric import CatchmentGeoJSON, NexusGeoJSON
 
 @pytest.fixture()
-def catchmentdata():
-    test_dir = Path(__file__).parent
-    test_file = test_dir/'data/hydrofabric/test_catchment_config.geojson'
+def testdir():
+    testdir = Path(__file__).parent
+    return testdir
+
+@pytest.fixture()
+def catchmentdata(testdir):
+    test_file = testdir/'data/hydrofabric/test_catchment_config.geojson'
     with open(test_file) as fp:
         catchmentdata = json.load(fp)
     return catchmentdata
 
-def test_catchment(catchmentdata):
-    NGenCatchmentFile(**catchmentdata)  
-
 @pytest.fixture()
-def nexusdata():
-    test_dir = Path(__file__).parent
-    test_file = test_dir/'data/hydrofabric/test_nexus_config.geojson'
+def nexusdata(testdir):
+    test_file = testdir/'data/hydrofabric/test_nexus_config.geojson'
     with open(test_file) as fp:
         nexusdata = json.load(fp)
     return nexusdata
 
-def test_nexus(nexusdata):
-    NGenNexusFile(**nexusdata)   
-
 @pytest.fixture()
-def realizationdata():
-    test_dir = Path(__file__).parent
-    test_file = test_dir/'data/test_config.json'
+def realizationdata(testdir):
+    test_file = testdir/'data/test_config.json'
     with open(test_file) as fp:
         realizationdata = json.load(fp)
-    realizationdata['routing']['t_route_config_file_with_path'] = test_dir/realizationdata['routing']['t_route_config_file_with_path']
+    realizationdata['routing']['t_route_config_file_with_path'] = testdir/realizationdata['routing']['t_route_config_file_with_path']
     return realizationdata
+
+def test_catchment(catchmentdata):
+    CatchmentGeoJSON(**catchmentdata)  
+
+def test_nexus(nexusdata):
+    NexusGeoJSON(**nexusdata)   
 
 def test_ngen_realization_config(realizationdata):
     g = NgenRealization(**realizationdata)
