0f58e04fb7da50de7d9b3f57c4a3c8768e5ce89b
Jordan Laser <jordan.science.laser@gmail.com>
2023-04-07 12:49:36 -0500

    moved validator out of model scope

--- python/ngen_conf/src/ngen/config/hydrofabric.py
+++ python/ngen_conf/src/ngen/config/hydrofabric.py
@@ -7,29 +7,21 @@ import re
 # match str that ends with a '-' followed by digits
 _TOID_REGEX_STR = ".\w+-\d+$"
 _TOID_REGEX = re.compile(_TOID_REGEX_STR)
+
+@validator('toid')
+def _validate_toid(cls, value: str) -> str:
+    if _TOID_REGEX.search(value) is None:
+        raise ValueError(f"Invalid 'toid' property value: {value!r}")
+    return value
     
 class CatchmentFeatureProperty(BaseModel):
     toid: str
     areasqkm : float
-
-    @validator('toid')
-    def _validate_toid(cls, value: str) -> str:
-        if _TOID_REGEX.search(value) is None:
-            raise ValueError(f"Invalid 'toid' property value: {value!r}")
-        return value
-    
     _validate_toid = validator("toid", allow_reuse=True)(_validate_toid)
-    
+
 class NexusFeatureProperty(BaseModel):
     toid: str
-
-    @validator('toid')
-    def _check_toid(cls, value: str) -> str:
-        if _TOID_REGEX.search(value) is None:
-            raise ValueError(f"Invalid 'toid' property value: {value!r}")
-        return value
-    
-    _check_toid = validator("toid", allow_reuse=True)(_check_toid)
+    _check_toid = validator("toid", allow_reuse=True)(_validate_toid)
 
 CatchmentGeoJSON = FeatureCollection[MultiPolygon, CatchmentFeatureProperty]
 NexusGeoJSON     = FeatureCollection[Point, NexusFeatureProperty]
