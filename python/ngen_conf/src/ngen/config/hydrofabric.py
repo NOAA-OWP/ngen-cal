3468d75dc8e122f17c272048be6574feafa9f2eb
Jordan Laser <jordan.science.laser@gmail.com>
2023-04-07 10:16:34 -0500

    Removed CSR classes and simplified geojson models

--- python/ngen_conf/src/ngen/config/hydrofabric.py
+++ python/ngen_conf/src/ngen/config/hydrofabric.py
@@ -1,74 +1,35 @@
 
 from typing import List, Literal
 from pydantic import BaseModel, validator
-from geojson_pydantic import Feature, Point,  MultiPolygon
+from geojson_pydantic import FeatureCollection, Point,  MultiPolygon
+import re
 
-def check_in_accepted(val, accepted):
-    if val not in accepted:
-        raise ValueError(f'Not an acceptable option. Options are {accepted}')
+# match str that ends with a '-' followed by digits
+_TOID_REGEX_STR = ".\D+-\d+$"
+_TOID_REGEX = re.compile(_TOID_REGEX_STR)
     
-class NexusFeatureProperty(BaseModel):
-    toid: str
-
-    @validator('toid')
-    def check_prefix(cls,val):
-        assert val[:3] == 'cat', "toid must be a cat id"
-
-NexusFeature = Feature[Point, NexusFeatureProperty]
-
 class CatchmentFeatureProperty(BaseModel):
     toid: str
     areasqkm : float
 
     @validator('toid')
-    def check_prefix(cls,val):
-        assert val[:3] == 'nex', "toid must be a nexus id"
+    def _validate_prefix(cls, value: str) -> str:
+        if _TOID_REGEX.search(value) is None:
+            raise ValueError(f"Invalid 'toid' property value: {value!r}")
+        return value
+    
+    _validate_prefix = validator("toid", allow_reuse=True)(_validate_prefix)
+    
+class NexusFeatureProperty(BaseModel):
+    toid: str
 
-CatchmentFeature = Feature[MultiPolygon, CatchmentFeatureProperty]
+    @validator('toid')
+    def _check_prefix(cls, value: str) -> str:
+        if _TOID_REGEX.search(value) is None:
+            raise ValueError(f"Invalid 'toid' property value: {value!r}")
+        return value
+    
+    _check_prefix = validator("toid", allow_reuse=True)(_check_prefix)
 
-class CRSProperty(BaseModel):
-    """
-    Model for CRS properties field in catchment or nexus data config
-    """    
-    name: Literal["urn:ogc:def:crs:OGC:1.3:CRS84"]
-
-    # Are any others accepted? If so, check with validator
-    # @validator('name')
-    # def check_properties(cls,val):
-    #     accepted = ["urn:ogc:def:crs:OGC:1.3:CRS84"] # Are any others accepted?
-    #     check_in_accepted(val, accepted)
-
-class CRS(BaseModel):
-    """
-    Model for CRS field in catchment or nexus data config
-    """
-    type: Literal["name"]
-    properties: CRSProperty
-
-    @validator('type')
-    def check_type(cls,val):
-        accepted = ["name"]
-        check_in_accepted(val, accepted)
-
-class NGenCatchmentFile(BaseModel):
-    """
-    Model for catchment data file
-    """
-
-    # required
-    type: Literal["FeatureCollection"]
-    name: Literal["catchment_data"]
-    crs: CRS
-    features: List[CatchmentFeature]
-
-
-class NGenNexusFile(BaseModel):
-    """
-    Model for nexus data file
-    """
-
-    # required
-    type: Literal["FeatureCollection"]
-    name: Literal["nexus_data"]
-    crs: CRS
-    features: List[NexusFeature]
+CatchmentGeoJSON = FeatureCollection[MultiPolygon, CatchmentFeatureProperty]
+NexusGeoJSON     = FeatureCollection[Point, NexusFeatureProperty]
