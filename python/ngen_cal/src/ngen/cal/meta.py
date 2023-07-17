305c875a3f307f18a1f1120f7df8b7a5aaa57632
hellkite500 <nels.frazier@noaa.gov>
2023-07-17 12:08:21 -0600

    add datetime (YYYYMMDDHHmm) to tmp dir prefix

--- python/ngen_cal/src/ngen/cal/meta.py
+++ python/ngen_cal/src/ngen/cal/meta.py
@@ -1,6 +1,7 @@
 import pandas as pd # type: ignore
 from pathlib import Path
 from tempfile import mkdtemp
+from datetime import datetime
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -16,11 +17,12 @@ class JobMeta:
 
         Args:
             name (str): name of the job, is used to construct log files
-            workdir (Path): working directory to stage the job under
+            workdir (Path): working directory to stage the job under.  If workdir is None, a temporary directory following the pattern
+                            YYYYMMDDHHmm_name_worker is created.  Once created, it is left to the user to cleanup if needed.
             log (bool, optional): Whether or not to create a log file for the job. Defaults to False.
         """
         if(workdir is None):
-            self._workdir = Path( mkdtemp(dir=parent_workdir, prefix=name+"_", suffix="_worker") ).resolve()
+            self._workdir = Path( mkdtemp(dir=parent_workdir, prefix=f"{datetime.now().strftime('%Y%m%d%H%M')}_{name}_", suffix="_worker") ).resolve()
         else:
             self._workdir = workdir
 
