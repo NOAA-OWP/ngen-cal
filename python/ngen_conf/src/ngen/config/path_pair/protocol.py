fd1158e69b6eee70a90b21f15d40523b379099f8
Austin Raney <austin.raney@noaa.gov>
2023-07-18 14:28:39 -0400

    add docstrings for protocols

--- python/ngen_conf/src/ngen/config/path_pair/protocol.py
+++ python/ngen_conf/src/ngen/config/path_pair/protocol.py
@@ -11,20 +11,32 @@ from .typing import T, S
 
 
 class Writer(Protocol):
+    """
+    Function that writes data to a path or elsewhere (e.g. network).
+    """
     def __call__(self, p: Path, data: bytes) -> None:
         ...
 
 
 class Reader(Protocol):
+    """
+    Function that reads data from a file path or elsewhere (e.g. network).
+    """
     def __call__(self, p: Path) -> bytes:
         ...
 
 
 class Deserializer(Protocol[T]):
+    """
+    Function that given bytes returns a T.
+    """
     def __call__(self, data: bytes) -> T:
         ...
 
 
 class Serializer(Protocol[S]):
+    """
+    Function that given a T returns bytes.
+    """
     def __call__(self, o: S) -> bytes:
         ...
