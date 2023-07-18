b04dd0b4b874756aa0cccec6670441553e457629
Austin Raney <austin.raney@noaa.gov>
2023-07-18 14:19:50 -0400

    add docstrings for generic typing parameters

--- python/ngen_conf/src/ngen/config/path_pair/typing.py
+++ python/ngen_conf/src/ngen/config/path_pair/typing.py
@@ -3,6 +3,8 @@ from typing import Union, TypeVar
 from typing_extensions import TypeAlias
 
 T = TypeVar("T", bound=object, covariant=True)
+"""Some type T or a subtype of T"""
 S = TypeVar("S", bound=object, contravariant=True)
+"""Some type S or a _supertype_ of S"""
 
 StrPath: TypeAlias = Union[Path, str]
