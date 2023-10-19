2f93cbf2cda1deb137ba38d3522d495dab4d54bb
Austin Raney <austin.raney@noaa.gov>
2023-10-19 10:34:24 -0400

    Monkey patch pydantic to allow schema serialization of PyObject types to string

--- python/ngen_conf/src/ngen/config/__init__.py
+++ python/ngen_conf/src/ngen/config/__init__.py
@@ -7,3 +7,10 @@ except ImportError:
     import typing
     from typing_extensions import Literal
     typing.Literal = Literal
+
+# Monkey patch pydantic to allow schema serialization of PyObject types to string
+from pydantic import PyObject
+def pyobject_schema(cls, field_schema):
+            field_schema['type'] = 'string'
+
+PyObject.__modify_schema__ = classmethod(pyobject_schema)
