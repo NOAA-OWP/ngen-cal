0d5169fe2126c95d094a1c37933b85d3d766affb
Austin Raney <austin.raney@noaa.gov>
2023-12-12 13:28:42 -0500

    add output_root test

--- python/ngen_conf/tests/test_realization.py
+++ python/ngen_conf/tests/test_realization.py
@@ -1,4 +1,5 @@
 import pytest
+from pathlib import Path
 from ngen.config.realization import Realization, NgenRealization
 from ngen.config.formulation import Formulation
 
@@ -28,3 +29,9 @@ def test_ngen_global_multi_realization(forcing, time, multi):
     # by writing the realization and then running `ngen` with it...
     # with open("test_realization_multi.json", 'w') as fp:
     #     fp.write( g.json(by_alias=True, exclude_none=True, indent=4))
+
+@pytest.mark.parametrize("forcing",["csv", "netcdf"], indirect=True )
+def test_ngen_global_realization_with_output_root(forcing, time, cfe):
+    f = Formulation(name=cfe.name, params=cfe)
+    r = Realization(formulations=[f], forcing=forcing)
+    g = NgenRealization(global_config=r, time=time, output_root=Path("/some/fake/path"))
