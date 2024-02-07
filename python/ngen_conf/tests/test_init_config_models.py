10aee29591ee33d84a0a9a9d5782214b0770d9d9
Austin Raney <austin.raney@noaa.gov>
2024-02-07 17:10:32 -0500

    test: soil moisture profile

--- python/ngen_conf/tests/test_init_config_models.py
+++ python/ngen_conf/tests/test_init_config_models.py
@@ -7,6 +7,7 @@ from ngen.config.init_config.cfe import CFE
 from ngen.config.init_config.noahowp import NoahOWP
 from ngen.config.init_config.pet import PET
 from ngen.config.init_config.soil_freeze_thaw import SoilFreezeThaw
+from ngen.config.init_config.soil_moisture_profile import SoilMoistureProfile
 
 
 def test_cfe(cfe_init_config: str):
@@ -105,3 +106,8 @@ def test_noah_owp_does_not_warns_if_soil_and_veg_type_are_water_or_neither_water
 def test_soil_freeze_thaw(soil_freeze_thaw_init_config: str):
     o = SoilFreezeThaw.from_ini_str(soil_freeze_thaw_init_config)
     assert o.to_ini_str() == soil_freeze_thaw_init_config
+
+
+def test_soil_moisture_profile(soil_moisture_profile_init_config: str):
+    o = SoilMoistureProfile.from_ini_str(soil_moisture_profile_init_config)
+    assert o.to_ini_str() == soil_moisture_profile_init_config
