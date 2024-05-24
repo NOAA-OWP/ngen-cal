95ff938dc2e2082ba6de777e420afc97471538b9
Austin Raney <austin.raney@noaa.gov>
2024-05-24 13:56:05 -0400

    feat: add optional 'plugins' and 'plugin_settings' keys to main ngen cal config

--- python/ngen_cal/src/ngen/cal/configuration.py
+++ python/ngen_cal/src/ngen/cal/configuration.py
@@ -1,20 +1,19 @@
-from __future__ import annotations #for pydnaitc 
+from __future__ import annotations #for pydnaitc
 
 import logging
 
 #Typing, datamodel
 from pydantic import BaseModel, Field, DirectoryPath
 from pathlib import Path
-from typing import Optional, Union
-try: #to get literal in python 3.7, it was added to typing in 3.8
-    from typing import Literal
-except ImportError:
-    from typing_extensions import Literal
+from typing_extensions import Literal
+from typing import Any, Dict, Optional, List, Union
+from types import ModuleType, FunctionType
 
 #local components for composing configuration
 from .strategy import Estimation, Sensitivity
 from .model import PosInt
 from .ngen import Ngen
+from .utils import PyObjectOrModule, type_as_import_string
 
 import logging
 logging.basicConfig(
@@ -39,6 +38,16 @@ class General(BaseModel):
     parameter_log_file: Optional[Path]
     objective_log_file: Optional[Path]
     random_seed: Optional[int]
+    plugins: List[PyObjectOrModule] = Field(default_factory=list)
+    plugin_settings: Dict[str, Dict[str, Any]] = Field(default_factory=dict)
+
+    class Config(BaseModel.Config):
+        # properly serialize plugins
+        json_encoders = {
+            type: type_as_import_string,
+            ModuleType: lambda mod: mod.__name__,
+            FunctionType: type_as_import_string,
+        }
 
 class NoModel(BaseModel):
     """
