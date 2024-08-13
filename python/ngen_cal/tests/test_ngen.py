05fc257168b077794988ed5739610b2d31dcbaaa
Austin Raney <austin.raney@noaa.gov>
2024-08-13 11:23:36 -0400

    test: validation error raised if 'output_root' provided

--- python/ngen_cal/tests/test_ngen.py
+++ python/ngen_cal/tests/test_ngen.py
@@ -23,7 +23,20 @@ def test_NgenIndependent_strategy_default_value():
     o = NgenIndependent.construct()
     assert o.strategy == NgenStrategy.independent
 
+
 def test_NgenUniform_strategy_default_value():
     # construct object without validation.
     o = NgenUniform.construct()
     assert o.strategy == NgenStrategy.uniform
+
+
+def test_NgenBase_verify_realization(ngen_config: Ngen):
+    # session level pytest fixture. take deep copy to avoid pollution
+    config = ngen_config.__root__.copy(deep=True)
+    assert isinstance(config, NgenBase)
+
+    assert config.ngen_realization is not None, "should have already raised if not None"
+    config.ngen_realization.output_root = pathlib.Path("./output_root")
+
+    with pytest.raises(pydantic.ValidationError):
+        Ngen.parse_obj(dict(config))
