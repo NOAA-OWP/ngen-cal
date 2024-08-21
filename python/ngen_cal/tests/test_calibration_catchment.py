0407eb9961acb53046c7c0ccc6d5e354873ccdf1
Austin Raney <austin.raney@noaa.gov>
2024-08-21 16:54:46 -0400

    style: formatting

--- python/ngen_cal/tests/test_calibration_catchment.py
+++ python/ngen_cal/tests/test_calibration_catchment.py
@@ -4,8 +4,9 @@ Test suite for calibratable_catchment
 
 from __future__ import annotations
 
-import pytest
 from typing import TYPE_CHECKING
+
+import pytest
 
 if TYPE_CHECKING:
     from ngen.cal.calibration_cathment import CalibrationCatchment
@@ -15,19 +16,25 @@ def test_df(catchment: CalibrationCatchment) -> None:
     """
     Test the catchments proper construction of the parameter dataframe
     """
-    assert catchment.df.iloc[0]['param'] == 'some_param'
-    assert catchment.df.iloc[0]['0'] == 0.5
-    assert catchment.df.iloc[0]['min'] == 0.0
-    assert catchment.df.iloc[0]['max'] == 1.0
+    assert catchment.df.iloc[0]["param"] == "some_param"
+    assert catchment.df.iloc[0]["0"] == 0.5
+    assert catchment.df.iloc[0]["min"] == 0.0
+    assert catchment.df.iloc[0]["max"] == 1.0
 
-def test_output(catchment2: CalibrationCatchment, monkeypatch: pytest.MonkeyPatch) -> None:
+
+def test_output(
+    catchment2: CalibrationCatchment,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
     """
     Test proper handling of non-existent output
     """
     import pandas as pd
+
     monkeypatch.setattr(pd, "read_csv", lambda *args, **kwargs: FileNotFoundError())
     output = catchment2.output
     assert output is None
+
 
 def test_observed(catchment: CalibrationCatchment) -> None:
     """
@@ -37,5 +44,6 @@ def test_observed(catchment: CalibrationCatchment) -> None:
     with pytest.raises(RuntimeError):
         catchment.observed
 
-#TODO test catchment_set
-#TODO test evaluation_range?
+
+# TODO test catchment_set
+# TODO test evaluation_range?
