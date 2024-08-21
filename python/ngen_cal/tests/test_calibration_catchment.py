e2b6c731064ed683477c7b5cd1df09919f2dfae0
Austin Raney <austin.raney@noaa.gov>
2024-08-21 16:54:04 -0400

    refactor: update type hints, remove unused variables, etc.

--- python/ngen_cal/tests/test_calibration_catchment.py
+++ python/ngen_cal/tests/test_calibration_catchment.py
@@ -1,41 +1,41 @@
+"""
+Test suite for calibratable_catchment
+"""
+
+from __future__ import annotations
+
 import pytest
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from ngen.cal.calibration_cathment import CalibrationCatchment
 
-"""
-    Test suite for calibratable_catchment
-"""
 
-@pytest.mark.usefixtures("catchment")
-def test_df(catchment: 'CalibrationCatchment') -> None:
+def test_df(catchment: CalibrationCatchment) -> None:
     """
-        Test the catchments proper construction of the parameter dataframe
+    Test the catchments proper construction of the parameter dataframe
     """
     assert catchment.df.iloc[0]['param'] == 'some_param'
     assert catchment.df.iloc[0]['0'] == 0.5
     assert catchment.df.iloc[0]['min'] == 0.0
     assert catchment.df.iloc[0]['max'] == 1.0
 
-@pytest.mark.usefixtures("catchment2")
-def test_output(catchment2: 'CalibrationCatchment', monkeypatch) -> None:
+def test_output(catchment2: CalibrationCatchment, monkeypatch: pytest.MonkeyPatch) -> None:
     """
-        Test proper handling of non-existent output
+    Test proper handling of non-existent output
     """
     import pandas as pd
     monkeypatch.setattr(pd, "read_csv", lambda *args, **kwargs: FileNotFoundError())
     output = catchment2.output
-    assert output == None
+    assert output is None
 
-@pytest.mark.usefixtures("catchment")
-def test_observed(catchment: 'CalibrationCatchment') -> None:
+def test_observed(catchment: CalibrationCatchment) -> None:
     """
-        Test proper handling of non-existent output
+    Test proper handling of non-existent output
     """
     catchment.observed = None
     with pytest.raises(RuntimeError):
-        obs = catchment.observed
+        catchment.observed
 
 #TODO test catchment_set
 #TODO test evaluation_range?
